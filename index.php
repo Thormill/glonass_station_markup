1d180eda8e2606d11146cf57abf737854b793d3b
Anton Shishkin <a.shishkin.b@gmail.com>
2011-09-30 14:17:21 +0400

    ash function

--- index.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" type="text/css" href="style/style.css" />
-<script type="text/javascript" src="scripts/jquery-min.js"></script>
-<script type="text/javascript" src="scripts/functions.js"></script>
-<form>
-<input type="text" id="longtitude"></input>
-<input type="text" id="anothertude"></input>
-<input type="button" onClick="toGeo();" value="go"></input>
-</form>
--- scripts/functions.js
+++ scripts/functions.js
@@ -4,3 +4,65 @@ function toGeo() {
     var val1 = coord1.split('.', 1);
     alert(val1);
 }
+
+/*
+* Исходные данные
+*/
+    h0 = 5;
+    h1 = [30] // массив высот вышек
+    h2 = 8;
+    N = -114; //dB
+    Pmin = Math.pow(10, N/10);
+    Pi = 12.5;
+    f = 162.025;
+    l = 300/f;
+    G1 = 0.81;
+    G2 = 0.81;
+/*
+* Для учета рельефа
+*/
+    H = [0.897, 0.99];          // отношение высоты препятствия к первой зоне Френеля
+    F0 = [0, -6.31, -20, -31];  // функция множителя ослабления
+/*
+* Функция для нахождения сигнала без помеж
+*/
+    function x(h1) {
+        return (Pi * G1 * G2 * (h1*h1 + h0*h0) * (h2*h2 + h0*h0) * l*l) / (2 * Pmin)
+    }
+/*
+* Функция получения максимального и минимального рельефа
+*/
+    function get(R) {
+        max = R[0];
+        min = R[0];
+        for (i = 1; i < R.length; i++) {
+            if (R[i] > max)
+                max = R[i];
+            if (R[i] < min)
+                min = R[i];
+        }
+        return [max, min];
+    }
+/*
+* Основной алгоритм
+*/
+    function calc() {
+        R = [];
+        for (i = 0; i < h1.length; i++) {
+            j = 0;
+            R.push([0,0,0,0,0,0,0,0]);
+            for (ii = 0; ii < H.length; ii++)
+                for (jj = 0; jj < F0.length; jj++) {
+                    // считаем с учетом рельефа
+                    f_h1 = (x(h1[i]) * Math.pow(10, 0.1*F0[jj]*H[ii])) / Math.abs(Math.log(2) * Math.pow(10,-4));
+                    R[i][j] = Math.pow(f_h1, 0.25);
+                    j++;
+                }
+        }
+        return R;
+    }
+    
+    $(document).ready( function() {
+        R = calc();
+        alert(get(R[0]));
+    })
