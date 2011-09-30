e7ab8f8b308e39990838cba6575040ee3a1344e0
Stepan Kirushkin <stepan.kirushkin@gmail.com>
2011-10-01 00:07:49 +0400

    rebased file system added ajax folder. started to create ajax request code

---
+++ ajax/getStations.php
@@ -0,0 +1,17 @@
+<?php
+/*define('ROOT', '../modules/');
+require_once ROOT . 'constants.php';
+require_once ROOT . 'database.class.php';
+$oDB = new Database($aDatabase['host'], $aDatabase['user'], $aDatabase['pwd'], $aDatabase['name']);
+
+$aStations = $oDB->selectTable('
+    SELECT ``, ``
+        FROM `stations`'
+);
+
+foreach ($aStations as $iStation => $aStation) {
+    //echo $aStation[''] . ';' . $aStation[''];
+    echo 30;
+}
+*/
+echo 30;
--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,31 @@
-function initialize() {
+aStations = [];
+
+$('#draw_btn').click( function() {
+    $.get('ajax/getStations.php', 
+        function (data) {
+            aStations = split(';',data);
+        });
+    map_draw_stations();
+})
+
+function map_init() {
     var myLatlng = new google.maps.LatLng(toGeo('57.35.50'), toGeo('39.54.50')); //1st elem
-    var myOptions = {zoom: 9, center: myLatlng, mapTypeId: google.maps.MapTypeId.ROADMAP}
-    map = new google.maps.Map(document.getElementById("my_map"), myOptions);
-    
+    var myOptions = {zoom: 10, center: myLatlng, mapTypeId: google.maps.MapTypeId.ROADMAP}
+    map = new google.maps.Map(document.getElementById('map'), myOptions);
+}
+
+function map_draw_stations() {
     R = calc();
         
 	maxR_opt = {fillColor: '00AAFF', fillOpacity: 0.5, strokeWeight: 0, clickable: false}
-   	minR_opt = {fillColor: "red", fillOpacity: 0.5, strokeWeight: 0, clickable: false}
+   	minR_opt = {fillColor: 'red', fillOpacity: 0.5, strokeWeight: 0, clickable: false}
     marker_opt = {map: map, icon: 'images/marker.png'}
     
     for(i = 0; i < R.length; i++){    
     /*отрисовка маркера*/
 		var point = new google.maps.LatLng(toGeo('57.35.50'),toGeo('39.54.50')); //ajax_me
 		marker_opt.position = point;
-        marker_opt.title = "Вышка #" + (i+1) + "\r\n" + "Коорд.: " + "\r\n" + "Мощность: ";
+        marker_opt.title = 'Вышка #' + (i+1) + "\r\n" + 'Коорд.: ' + "\r\n" + 'Мощность: ';
         marker_opt.size
         var marker = new google.maps.Marker(marker_opt);
 	/*отрисовка радиуса*/
@@ -29,9 +41,3 @@ function initialize() {
 		circle.setMap(map);
 	}
 }
-function loadScript() {
-    var script = document.createElement("script");
-    script.src = "http://maps.google.com/maps/api/js?sensor=false&callback=initialize";
-    document.body.appendChild(script);
-}
-window.onload = loadScript;
