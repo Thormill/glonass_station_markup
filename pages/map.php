5b5b708e7a5f81a64e9af771f75b274033cf9236
Stepan Kirushkin <stepan.kirushkin@gmail.com>
2011-10-01 12:55:49 +0400

    over 9000

---
+++ pages/map.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&callback=map_init"></script>
+
+<input type="button" value="Отобразить БС на карте" onclick="draw_btn_click()">
+<div id="gmap" style="width:1000; height:600px;"></div>
---
+++ scripts/map.js
@@ -0,0 +1,45 @@
+aStations = [];
+
+function draw_btn_click() {
+    $.post('ajax/getStations.php', 
+        function (data) {
+            aStations = data.split(';');
+//            alert(aStations);
+            h1 = aStations;
+            map_draw_stations();
+        });
+}
+
+function map_init() {
+    var myLatlng = new google.maps.LatLng(toGeo('57.35.50'), toGeo('39.54.50')); //1st elem
+    var myOptions = {zoom: 10, center: myLatlng, mapTypeId: google.maps.MapTypeId.ROADMAP}
+    map = new google.maps.Map(document.getElementById('gmap'), myOptions);
+}
+
+function map_draw_stations() {
+    R = calc();
+        
+	maxR_opt = {fillColor: '#00AAFF', fillOpacity: 0.5, strokeWeight: 0, clickable: false}
+   	minR_opt = {fillColor: 'red', fillOpacity: 0.5, strokeWeight: 0, clickable: false}
+    marker_opt = {map: map, icon: 'images/marker.png'}
+    
+    for(i = 0; i < R.length; i++){    
+    /*отрисовка маркера*/
+		var point = new google.maps.LatLng(toGeo('57.35.50'),toGeo('39.54.50')); //ajax_me
+		marker_opt.position = point;
+        marker_opt.title = 'Вышка #' + (i+1) + "\r\n" + 'Коорд.: ' + "\r\n" + 'Мощность: ';
+        marker_opt.size
+        var marker = new google.maps.Marker(marker_opt);
+	/*отрисовка радиуса*/
+		maxR_opt.center = minR_opt.center = point;    
+		radius = get(R[i]);
+
+		maxR_opt.radius = radius[0] / 100;
+		circle = new google.maps.Circle(maxR_opt);
+		circle.setMap(map);
+		
+		minR_opt.radius = radius[1] / 100;
+		circle = new google.maps.Circle(minR_opt);
+		circle.setMap(map);
+	}
+}
