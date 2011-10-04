78eb5d82a420863c5caf91d6f20cf4127eb146fb
Stepan Kirushkin <stepan@leadsleader.ru>
2011-10-04 10:29:20 +0400

    some modifications for style

--- pages/pmap.php
+++ pages/pmap.php
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&callback=map_init"></script>
 
 <input type="checkbox" id="show_markers" checked="checked" />Маркеры
-<input type="button" onclick="draw_btn_click()" value="go" />
+<input type="button" onclick="draw_btn_click()" value="Отрисовать БС" />
 <div id="map" style="width:1000; height:600px;"></div>
