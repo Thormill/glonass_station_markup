4b82b1f91e8f16e513303568cb7d0021e59435ce
Stepan Kirushkin <stepan@leadsleader.ru>
2011-10-04 10:55:35 +0400

    math fix

--- scripts/map.js
+++ scripts/map.js
@@ -42,8 +42,8 @@ function map_draw_stations() {
             aMarkerOpt.title = 'Вышка #' + (i+1) + "\r\n" +
                 'Мощность: ' + aStations[i][2] + "кВт \r\n" +
                 'Высота: ' + aStations[i][3] + "м \r\n" +
-                'R max: ' + Math.floor(aRadius[0]) + "м \r\n" +
-                'R min: ' + Math.floor(aRadius[1]) + "м";
+                'R max: ' + Math.floor(aRadius[0]*100)/100 + "м \r\n" +
+                'R min: ' + Math.floor(aRadius[1]*100)/100 + "м";
             var oMarker = new google.maps.Marker(aMarkerOpt);
         }
 	/*отрисовка радиуса*/
