ec622d1286f1b246e2c6081799f0390e0625fad2
Anton Shishkin <a.shishkin.b@gmail.com>
2011-10-04 10:28:22 +0400

    bug fix

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,7 @@ function Validate(num) {
 
  	if(reg.test(data[0]))
 	    if(reg.test(data[1]))
-	        Edit(num);
+	        Save(num);
 	    else 
 	        alert('error in (E) ' + data[1]);
 	else
@@ -26,8 +26,7 @@ function Validate(num) {
 }
 
 function Edit(num) {
-    var tmp = $('#coord' + num).html();
-    $('#coord' + num).html('<input type="text" value="' + tmp + '" id="e_coord' + num + '" / >');
+    $('#coord' + num).html('<input type="text" value="' + $('#coord' + num).html() + '" id="e_coord' + num + '" / >');
     $('#power' + num).html('<input type="text" value="' + $('#power' + num).html() + '" id="e_power' + num + '" / >');
     $('#k' + num).html('<input type="text" value="' + $('#k' + num).html() + '" id="e_k' + num + '"></input>');
     $('#height' + num).html('<input type="text" value="' + $('#height' + num).html() + '" id="e_heights' + num + '" / >');
