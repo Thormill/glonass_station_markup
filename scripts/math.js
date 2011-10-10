1f330bf71f79ae63f535b2a95aff02385df96f4a
Stepan Kirushkin <stepan@leadsleader.ru>
2011-10-10 13:34:07 +0400

    new mathematic

--- scripts/math.js
+++ scripts/math.js
@@ -9,7 +9,7 @@
 h0 = 5;
 h1 = 0;                    // высота вышки
 h2 = 8;
-N = -114;                   //dB
+N = -114;                   // dB
 Pmin = Math.pow(10, N/10);
 Pi = 0;                     // Вт мощность  передатчика
 f = 162.025;                // частота передачи
@@ -30,8 +30,8 @@ function setVariables(_h1, _Pi) {
 /*
 * Функция для нахождения распространения сигнала без помех
 */
-function x(_h1) {
-    return (Pi * G1 * G2 * (_h1*_h1 + h0*h0) * (h2*h2 + h0*h0) * l*l) / (2 * Pmin)
+function X() {
+    return (Pi * G1 * G2 * (h1*h1 + h0*h0) * (h2*h2 + h0*h0) * l*l) / (2 * Pmin * 1000)
 }
 /*
 * Функция получения максимального и минимального рельефа
@@ -53,8 +53,8 @@ function calcRadiuses() {
     i = 0;
     for (var ii = 0; ii < H.length; ii++)
         for (var jj = 0; jj < F0.length; jj++) {
-            // считаем с учетом рельефа
-            f_h1 = (x(h1) * Math.pow(10, 0.1*F0[jj]*H[ii])) / Math.abs(Math.log(2) * Math.pow(10,-4));
+            // считаем с учетом рельефа для h1
+            f_h1 = (X() * Math.pow(10, 0.1*F0[jj]*H[ii])) / Math.abs(Math.log(2) * Math.pow(10,-4));
             R[i] = Math.pow(f_h1, 0.25);
             i++;
         }
@@ -67,4 +67,43 @@ function toGeo(str) {
 	tmp = str.split('.', 3);
 	result = parseFloat(parseFloat(tmp[0]) + (((parseFloat(tmp[1]) * 60) + parseFloat(tmp[2])) / 3600));
 	return result;
+}
+/*
+* Вычисление интеграла по dy от выражения expression
+*/
+function integral(expression, a, b) {
+    if (!expression || !b && !a) return 0;
+    if (a == b) return 0;
+
+    var y, x1, x2, n, length, dy, S=0;
+
+    length = Math.abs(b - a);
+    x1 = Math.min(a, b); 
+    b = Math.max(a, b);  
+    a = x1;
+    n = 100;
+    if (length>2)
+        n = Math.round(100 * Math.log(length + 1));
+    dy = length/n;
+    y = a;
+    x1 = eval(expression);
+    y = a + dy;
+    x2 = eval(expression);
+    S = (x1 + x2) * dy/2;
+    for(i=2; i<=n; i++) {
+        x1 = x2;
+        y = y + dy;
+        x2 = eval(expression);
+        S += (x1 + x2) * dy/2;
+    }
+    return S
+}
+/*
+* Вычисление вероятности ошибки от заданного R1
+*/
+function Perr(R1) {
+    return (8/Math.PI * Math.pow(X(),0.75)/Math.pow(R1,3)) *
+        integral('Math.pow(y,0.5) * Math.exp(-(1/Math.pow(y,2) + Math.pow(X(),0.25)*y/(Math.PI*' + (R1*R1) + ')))', 0, Math.pow(10,10.5));
+//    return (8/Math.PI * Math.pow(X(),3/4)/Math.pow(R1,3)) *
+//        integral("Math.pow(y,0.5) * Math.exp(-(1/Math.pow(y,2) + Math.pow(X(),0.25)*y/(Math.PI*" + (R1*R1) + ")))", 0, 1);
 }
